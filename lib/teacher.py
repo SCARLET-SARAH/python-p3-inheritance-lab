c5990fc63be291f9597f457ba26520467820d17c
scarlet <sarahscarlet641@gmail.com>
2024-06-03 10:04:36 +0300

    complete

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,16 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = self.generate_knowledge()
 
+    def generate_knowledge(self):
+        # Generate some initial knowledge for the teacher
+        knowledge = ["Subject A", "Subject B", "Subject C"]
+        return knowledge
+    
     def teach(self):
-        pass
+        # Randomly select a topic from the teacher's knowledge
+        topic = random.choice(self.knowledge)
+        return topic
